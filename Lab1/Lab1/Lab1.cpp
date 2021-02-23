8c9335d758ff8ed477d600e1045742f62633375c
Уляна Деркач <uliana.derkach@nure.ua>
2021-02-23 13:59:49 +0300

    created classes Node and SortedLinkedList

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -1,5 +1,45 @@
 ﻿#include <iostream>
 
+class Node
+{
+public:
+    int Value;
+    Node* Next;
+
+    Node(int val = -1, Node* next = NULL);
+};
+
+Node::Node(int val, Node* next)
+{
+    Value = val;
+    Next = next;
+}
+
+class SortedLinkedList
+{
+public:
+    SortedLinkedList(int size = 0, Node* first = NULL);
+    bool IsFull();
+    bool IsEmpty();
+    void MakeEmpty();
+    void AddItem(int item);
+    int DeleteItem(int item);
+    int Search(int item);
+    int Retrieve(int item);
+    void PrintList();
+    int GetSize();
+
+private:
+    int Size;
+    Node* First;
+};
+
+SortedLinkedList::SortedLinkedList(int size, Node* first)
+{
+    Size = size;
+    First = first;
+}
+
 int main()
 {
     
