e5237ce0e12722a11193ff94593b44f21bab7052
Уляна Деркач <uliana.derkach@nure.ua>
2021-02-27 21:05:51 +0300

    created PrintInorder(), PrintPostorder() and PrintPreorder() methods

--- Lab 2/Lab 2/Lab 2.cpp
+++ Lab 2/Lab 2/Lab 2.cpp
@@ -25,6 +25,15 @@ public:
 
     bool IsEmpty();
     bool IsFull();
+
+    void PrintPreorder();
+    void PrintInorder();
+    void PrintPostorder();
+
+private:
+    void PrintPreorder(Node* next);
+    void PrintInorder(Node* next);
+    void PrintPostorder(Node* next);
 };
 
 BBST::BBST(Node* root)
@@ -44,6 +53,51 @@ bool BBST::IsFull()
     delete temp;
 
     return result;
+}
+
+void BBST::PrintPreorder()
+{
+    PrintPreorder(Root);
+}
+
+void BBST::PrintPreorder(Node* next)
+{
+    if (next)
+    {
+        std::cout << next->Value << " ";
+        PrintPreorder(next->Left);
+        PrintPreorder(next->Right);
+    }
+}
+
+void BBST::PrintInorder()
+{
+    PrintInorder(Root);
+}
+
+void BBST::PrintInorder(Node* next)
+{
+    if (next)
+    {
+        PrintInorder(next->Left);
+        std::cout << next->Value << " ";
+        PrintInorder(next->Right);
+    }
+}
+
+void BBST::PrintPostorder()
+{
+    PrintPostorder(Root);
+}
+
+void BBST::PrintPostorder(Node* next)
+{
+    if (next)
+    {
+        PrintPostorder(next->Left);
+        PrintPostorder(next->Right);
+        std::cout << next->Value << " ";
+    }
 }
 
 int main()
