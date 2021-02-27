46f8666a3cf961b0c79342f301d27b7fc990c0a1
Уляна Деркач <uliana.derkach@nure.ua>
2021-02-27 20:42:41 +0300

    created classes Node and BBST

--- Lab 2/Lab 2/Lab 2.cpp
+++ Lab 2/Lab 2/Lab 2.cpp
@@ -1,5 +1,34 @@
 ﻿#include <iostream>
 
+class Node
+{
+public:
+    int Value;
+    Node* Left;
+    Node* Right;
+
+    Node(int value = 0, Node* l = NULL, Node* r = NULL);
+};
+
+Node::Node(int value, Node* l, Node* r)
+{
+    Value = value;
+    Left = l;
+    Right = r;
+}
+
+class BBST
+{
+public:
+    Node* Root;
+    BBST(Node* root = NULL);
+};
+
+BBST::BBST(Node* root)
+{
+    Root = root;
+}
+
 int main()
 {
     
