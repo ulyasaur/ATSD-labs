02baa38154bcbc79f387b97db42a578053bf810d
Уляна Деркач <uliana.derkach@nure.ua>
2021-04-11 13:38:08 +0300

    created PrintList() and IsFull()

--- Lab 3/Lab 3/Lab 3.cpp
+++ Lab 3/Lab 3/Lab 3.cpp
@@ -6,10 +6,14 @@ class List
 public:
     List(int size = 1);
 
+    void PrintList();
+
 private:
     int Size;
     int LastItem;
     T* Values;
+
+    bool IsFull();
 };
 
 template <class T>
@@ -20,7 +24,23 @@ List<T>::List(int size)
     Values = new T[size];
 }
 
+template <class T>
+void List<T>::PrintList()
+{
+    for (int i = 0; i <= LastItem; i++)
+    {
+        std::cout << Values[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+template <class T>
+bool List<T>::IsFull()
+{
+    return LastItem + 1 == Size;
+}
+
 int main()
 {
-    
+
 }
