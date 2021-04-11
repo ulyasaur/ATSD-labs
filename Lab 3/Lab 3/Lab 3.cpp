70e49ab7488b329915e49afb4b17f8d3669d01a4
Уляна Деркач <uliana.derkach@nure.ua>
2021-04-11 21:00:00 +0300

    created Heapsort with minheap and maxheap

--- Lab 3/Lab 3/Lab 3.cpp
+++ Lab 3/Lab 3/Lab 3.cpp
@@ -7,6 +7,8 @@ public:
     List(int size = 1);
 
     void AddItem(T item);
+    void HeapsortMax();
+    void HeapsortMin();
     void PrintList();
     int Size();
     bool IsFull();
@@ -17,6 +19,8 @@ private:
     T* Values;
 
     void DoubleSize();
+    void HeapsortMax(int index, int size);
+    void HeapsortMin(int index, int size);
 };
 
 template <class T>
@@ -52,6 +56,100 @@ void List<T>::AddItem(T item)
 }
 
 template <class T>
+void List<T>::HeapsortMax()
+{
+    int size = LastItem + 1;
+
+    for (int i = size / 2 - 1; i >= 0; i--)
+    {
+        HeapsortMax(size, i);
+    }
+
+    for (int i = size - 1; i > 0; i--) 
+    {
+        T temp = Values[i];
+        Values[i] = Values[0];
+        Values[0] = temp;
+
+        HeapsortMax(i, 0);
+    }
+}
+
+template <class T>
+void List<T>::HeapsortMax(int size, int index)
+{
+    int largest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if (left < size && Values[left] > Values[largest])
+    {
+        largest = left;
+    }
+    
+    if (right < size && Values[right] > Values[largest])
+    {
+        largest = right;
+    }
+
+    if (largest != index)
+    {
+        T temp = Values[index];
+        Values[index] = Values[largest];
+        Values[largest] = temp;
+
+        HeapsortMax(size, largest);
+    }
+}
+
+template <class T>
+void List<T>::HeapsortMin()
+{
+    int size = LastItem + 1;
+
+    for (int i = size / 2 - 1; i >= 0; i--)
+    {
+        HeapsortMin(size, i);
+    }
+
+    for (int i = size - 1; i > 0; i--) 
+    {
+        T temp = Values[i];
+        Values[i] = Values[0];
+        Values[0] = temp;
+
+        HeapsortMin(i, 0);
+    }
+}
+
+template <class T>
+void List<T>::HeapsortMin(int size, int index)
+{
+    int smallest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if (left < size && Values[left] < Values[smallest])
+    {
+        smallest = left;
+    }
+    
+    if (right < size && Values[right] < Values[smallest])
+    {
+        smallest = right;
+    }
+
+    if (smallest != index)
+    {
+        T temp = Values[index];
+        Values[index] = Values[smallest];
+        Values[smallest] = temp;
+
+        HeapsortMin(size, smallest);
+    }
+}
+
+template <class T>
 void List<T>::PrintList()
 {
     for (int i = 0; i <= LastItem; i++)
@@ -75,5 +173,17 @@ bool List<T>::IsFull()
 
 int main()
 {
+    List<int> list;
+    int n;
+    std::cin >> n;
 
+    while (n != -1)
+    {
+        list.AddItem(n);
+        std::cin >> n;
+    }
+
+    list.PrintList();
+    list.HeapsortMin();
+    list.PrintList();
 }
