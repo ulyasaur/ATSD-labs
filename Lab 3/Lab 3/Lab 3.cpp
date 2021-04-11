089db6d414a1e93dc10f06fc67d8c29261d4417a
Уляна Деркач <uliana.derkach@nure.ua>
2021-04-11 13:32:17 +0300

    created class List and constructor

--- Lab 3/Lab 3/Lab 3.cpp
+++ Lab 3/Lab 3/Lab 3.cpp
@@ -1,5 +1,25 @@
 ﻿#include <iostream>
 
+template <class T>
+class List
+{
+public:
+    List(int size = 1);
+
+private:
+    int Size;
+    int LastItem;
+    T* Values;
+};
+
+template <class T>
+List<T>::List(int size)
+{
+    Size = size;
+    LastItem = -1;
+    Values = new T[size];
+}
+
 int main()
 {
     
