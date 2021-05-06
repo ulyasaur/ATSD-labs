2c24a06bd591aa4f3a6eb14d4c5d4cac6008ee9d
Уляна Деркач <uliana.derkach@nure.ua>
2021-05-06 18:25:45 +0300

    created class Node

--- Lab 4/Lab 4/Lab 4.cpp
+++ Lab 4/Lab 4/Lab 4.cpp
@@ -1,4 +1,24 @@
 ﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Node
+{
+public:
+    string Name;
+    int Value;
+    Node* Next;
+
+    Node(string name = "",  int val = -1, Node* next = NULL);
+};
+
+Node::Node(string name, int val, Node* next)
+{
+    Name = name;
+    Value = val;
+    Next = next;
+}
 
 int main()
 {
