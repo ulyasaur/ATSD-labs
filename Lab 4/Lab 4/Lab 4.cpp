858f49c5a5221b13074f58581b2dd3559089e134
Уляна Деркач <uliana.derkach@nure.ua>
2021-05-06 18:53:30 +0300

    created class AdjList with AddItem() method

--- Lab 4/Lab 4/Lab 4.cpp
+++ Lab 4/Lab 4/Lab 4.cpp
@@ -7,19 +7,46 @@ class Node
 {
 public:
     string Name;
-    int Value;
+    int Weight;
     Node* Next;
 
-    Node(string name = "",  int val = -1, Node* next = NULL);
+    Node(string name = "",  int val = 0, Node* next = NULL);
 };
 
 Node::Node(string name, int val, Node* next)
 {
     Name = name;
-    Value = val;
+    Weight = val;
     Next = next;
+}
+
+class AdjList
+{
+public:
+    AdjList(Node* first = NULL);
+    void AddItem(Node* node);
+
+private:
+    Node* First;
+};
+
+AdjList::AdjList(Node* first)
+{
+    First = first;
+}
+
+void AdjList::AddItem(Node* node)
+{
+    Node* temp = First;
+    while (temp->Next)
+    {
+        temp = temp->Next;
+    }
+
+    temp->Next = node;
 }
 
 int main()
 {
+
 }
